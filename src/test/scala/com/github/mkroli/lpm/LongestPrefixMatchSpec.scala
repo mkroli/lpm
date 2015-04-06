2d90a65365cafdf83b4c1134137aa145c34fb687
Michael Krolikowski <mkroli@yahoo.de>
2015-04-06 19:07:36 +0200

    use compact() in tests

--- src/test/scala/com/github/mkroli/lpm/LongestPrefixMatchSpec.scala
+++ src/test/scala/com/github/mkroli/lpm/LongestPrefixMatchSpec.scala
@@ -14,107 +14,116 @@
  * limitations under the License.
  */
 package com.github.mkroli.lpm
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSpec
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class LongestPrefixMatchSpec extends FunSpec {
+  def test[T](lpm: LongestPrefixMatch[T])(t: (LongestPrefixMatch[T]) => Unit) =
+    List(lpm, lpm.compact()).foreach(t)
+
   describe("LongestPrefixMatch") {
     it("should return None if nothing is found") {
       val lpm = new LongestPrefixMatch[Int]()
-      assert(None == lpm.getValueFromPrefix("789"))
-      val lpm2 = lpm.addValueForRange("123", "456", 1)
-      assert(None == lpm2.getValueFromPrefix("789"))
+      test(lpm) { lpm =>
+        assert(None == lpm.getValueFromPrefix("789"))
+      }
+      test(lpm.addValueForRange("123", "456", 1)) { lpm =>
+        assert(None == lpm.getValueFromPrefix("789"))
+      }
     }
 
     it("should return value no matter how deep input is") {
-      val lpm = new LongestPrefixMatch[Int]().
-        addValueForRange("123", "456", 1)
-      assert(1 == lpm.getValueFromPrefix("33333333333333333333").get)
+      test(new LongestPrefixMatch[Int]()
+        .addValueForRange("123", "456", 1)) { lpm =>
+        assert(1 == lpm.getValueFromPrefix("3" * 1024).get)
+      }
     }
 
     it("should return value on exact matches") {
-      val lpm = new LongestPrefixMatch[Int]().
-        addValueForRange("123", "456", 1)
-      assert(None == lpm.getValueFromPrefix("122"))
-      assert(1 == lpm.getValueFromPrefix("123").get)
-      assert(1 == lpm.getValueFromPrefix("456").get)
-      assert(None == lpm.getValueFromPrefix("457"))
+      test(new LongestPrefixMatch[Int]()
+        .addValueForRange("123", "456", 1)) { lpm =>
+        assert(None == lpm.getValueFromPrefix("122"))
+        assert(1 == lpm.getValueFromPrefix("123").get)
+        assert(1 == lpm.getValueFromPrefix("456").get)
+        assert(None == lpm.getValueFromPrefix("457"))
+      }
     }
 
     it("should return longest prefix matching on overlap") {
-      val lpm = new LongestPrefixMatch[Int]().
-        addValueForRange("3123", "3123", 1).
-        addValueForRange("312", "312", 2)
-      assert(1 == lpm.getValueFromPrefix("3123").get)
-      assert(2 == lpm.getValueFromPrefix("3122").get)
+      test(new LongestPrefixMatch[Int]()
+        .addValueForRange("3123", "3123", 1)
+        .addValueForRange("312", "312", 2)) { lpm =>
+        assert(1 == lpm.getValueFromPrefix("3123").get)
+        assert(2 == lpm.getValueFromPrefix("3122").get)
+      }
     }
 
     it("should return longest prefix matching as if it wasn't optimized") {
-      val lpm = new LongestPrefixMatch[Int]().
+      test(new LongestPrefixMatch[Int]().
         addValueForRange("100", "199", 1).
-        addValueForRange("15", "19", 2)
-      assert(1 == lpm.getValueFromPrefix("17").get)
+        addValueForRange("15", "19", 2)) { lpm =>
+        assert(1 == lpm.getValueFromPrefix("17").get)
+      }
     }
 
     it("should return longest not latest prefix even if optimized") {
       val lpm = new LongestPrefixMatch[Int]().
         addValueForRange("1", "1", 2).
         addValueForRange("100", "199", 1)
-      assert(1 == lpm.getValueFromPrefix("1").get)
-      val lpm2 = lpm.addValueForRange("200", "299", 1).
-        addValueForRange("2", "2", 2)
-      assert(1 == lpm2.getValueFromPrefix("2").get)
+      test(lpm) { lpm =>
+        assert(1 == lpm.getValueFromPrefix("1").get)
+      }
+      test(lpm.addValueForRange("200", "299", 1).
+        addValueForRange("2", "2", 2)) { lpm =>
+        assert(1 == lpm.getValueFromPrefix("2").get)
+      }
     }
 
     it("should accept a range without any prefix (0-9)") {
-      val lpm1 = new LongestPrefixMatch[Int]().
-        addValueForRange("0", "9", 1)
-      val lpm2 = new LongestPrefixMatch[Int]().
-        addValueForRange("00", "99", 1)
-      for (i <- 0 to 999) {
-        assert(1 == lpm1.getValueFromPrefix(i.toString).get)
-        assert(1 == lpm2.getValueFromPrefix(i.toString).get)
+      test(new LongestPrefixMatch[Int]().addValueForRange("0", "9", 1)) { lpm1 =>
+        test(new LongestPrefixMatch[Int]().addValueForRange("00", "99", 1)) { lpm2 =>
+          for (i <- 0 to 999) {
+            assert(1 == lpm1.getValueFromPrefix(i.toString).get)
+            assert(1 == lpm2.getValueFromPrefix(i.toString).get)
+          }
+        }
       }
     }
 
     it("should accept a top-level range (1-9)") {
-      val lpm = new LongestPrefixMatch[Int]().
-        addValueForRange("1", "9", 1)
-      assert(None == lpm.getValueFromPrefix("0"))
-      for (i <- 1 to 9)
-        assert(1 == lpm.getValueFromPrefix(i.toString).get)
+      test(new LongestPrefixMatch[Int]().
+        addValueForRange("1", "9", 1)) { lpm =>
+        assert(None == lpm.getValueFromPrefix("0"))
+        for (i <- 1 to 9)
+          assert(1 == lpm.getValueFromPrefix(i.toString).get)
+      }
     }
 
     it("should accept a top-level range (0-8)") {
-      val lpm = new LongestPrefixMatch[Int]().
-        addValueForRange("0", "8", 1)
-      for (i <- 0 to 8)
-        assert(1 == lpm.getValueFromPrefix(i.toString).get)
-      assert(None == lpm.getValueFromPrefix("9"))
+      test(new LongestPrefixMatch[Int]().
+        addValueForRange("0", "8", 1)) { lpm =>
+        for (i <- 0 to 8)
+          assert(1 == lpm.getValueFromPrefix(i.toString).get)
+        assert(None == lpm.getValueFromPrefix("9"))
+      }
     }
 
     it("should not accept invalid ranges") {
-      val lpm = new LongestPrefixMatch[Int]()
-      intercept[IllegalArgumentException](lpm.addValueForRange("", "", 1))
-      intercept[IllegalArgumentException](lpm.addValueForRange("a", "b", 2))
-      intercept[IllegalArgumentException](lpm.addValueForRange("1", "23", 3))
-    }
-
-    it("shouldn't care about ranges with different weights at all") {
-      val lpm = new LongestPrefixMatch[Int]().
-        addValueForRange("1", "1", 1).
-        addValueForRange("10", "19", 2).
-        addValueForRange("111", "189", 3).
-        addValueForRange("20", "29", 4).
-        addValueForRange("2", "2", 5)
+      test(new LongestPrefixMatch[Int]()) { lpm =>
+        intercept[IllegalArgumentException](lpm.addValueForRange("", "", 1))
+        intercept[IllegalArgumentException](lpm.addValueForRange("a", "b", 2))
+        intercept[IllegalArgumentException](lpm.addValueForRange("1", "23", 3))
+      }
     }
 
     it("should be accessable using shorter methods") {
-      val lpm = new LongestPrefixMatch[Int] + ("1", "5", 1) + ("6", "9", 2)
-      assert(1 === lpm("123").get)
-      assert(2 === lpm("789").get)
+      test(new LongestPrefixMatch[Int] + ("1", "5", 1) + ("6", "9", 2)) { lpm =>
+        assert(1 === lpm("123").get)
+        assert(2 === lpm("789").get)
+      }
     }
   }
 }
